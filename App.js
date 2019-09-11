d63fff170ad2314564ff0b478631bafff34083d0
Muhammad <m.haris.abdurrahman@gmail.com>
2019-09-11 12:05:57 +0700

    upgrade ui-kitten to v4.2.0-beta.2

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import StorybookUI from './storybook';
 import { mapping, light as lightTheme } from '@eva-design/eva';
-import { ApplicationProvider } from 'react-native-ui-kitten';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
+import { ApplicationProvider,  IconRegistry } from 'react-native-ui-kitten';
 
 const App = () => (
   <ApplicationProvider
     mapping={mapping}
     theme={lightTheme}>
+      <IconRegistry icons={EvaIconsPack}/>
       <StorybookUI/>
   </ApplicationProvider>
 );
