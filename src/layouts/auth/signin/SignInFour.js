0a4c2d062cbadaf5494a9e8eeb3dc53da0397ce8
Muhammad <m.haris.abdurrahman@gmail.com>
2019-09-11 18:15:28 +0700

    Complete sign in option 4

--- src/layouts/auth/signin/SignInFour.js
+++ src/layouts/auth/signin/SignInFour.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
-import { View, Text, ImageBackground, StyleSheet } from 'react-native';
-import { Button, Layout, Input } from 'react-native-ui-kitten';
+import { View, ImageBackground, StyleSheet, TouchableOpacity, KeyboardAvoidingView } from 'react-native';
+import { Button, Layout, Input, Text, Icon } from 'react-native-ui-kitten';
+
 
 class SignInFour extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            passwordVisible: false,
+        };
     }
-
-
+    onPasswordIconPress = () => {
+        const passwordVisible = !this.state.passwordVisible;
+        this.setState({ passwordVisible });
+    };
 
     render() {
         const {
@@ -16,6 +22,9 @@ class SignInFour extends Component {
             validation,
             btnText,
             onPress,
+            onPressGoogle,
+            onPressFacebook,
+            onPressTwitter
         } = this.props;
         const {
             emailPlaceholder,
@@ -24,60 +33,181 @@ class SignInFour extends Component {
             password,
         } = form
         return (
-            <Layout
+            <ImageBackground
                 style={styles.wrapper}
+                resizeMode='stretch'
+                source={{ uri: bgUrl }}
             >
-                <ImageBackground
-                    resizeMode='stretch'
-                    style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
-                    source={{ uri: bgUrl }}>
-                    <View >
+                <Layout
+                    style={styles.titleContainer}
+                >
+                    <Text
+                        category='h1'
+                        style={styles.titleText}
+                    >
+                        Hello
+                </Text>
+                    <Text
+                        category='h1'
+                        style={[styles.titleText, { fontSize: 15, fontWeight: '200' }]}
+                    >
+                        Sign in to your account
+                </Text>
+                </Layout>
+                <Layout
+                    style={styles.formContainer}
+                >
+                    <View>
                         <Input
-                            label='Email'
                             placeholder={emailPlaceholder}
                             value={email}
+                            size={'small'}
                             style={styles.input}
+                            icon={(style) => (<Icon name='email-outline' {...style} />)}
                         />
                         <Input
-                            label='Password'
                             placeholder={passwordPlaceholder}
                             secureTextEntry={true}
                             value={password}
-                            style={{ width: '75%', marginTop: '2%' }}
+                            size={'small'}
+                            style={styles.input}
+                            icon={(style) => (
+                                <Icon
+                                    name={this.state.passwordVisible ? 'eye' : 'eye-off'}
+                                    {...style}
+                                />
+                            )
+                            }
+                            onIconPress={this.onPasswordIconPress}
+                            secureTextEntry={!this.state.passwordVisible}
                         />
+                        <TouchableOpacity>
+                            <Text style={[styles.text, { alignSelf: 'flex-end' }]}>Forgot your password? </Text>
+                        </TouchableOpacity>
                     </View>
-                    <Button
-                        onPress={onPress()}
-                        size="giant" style={styles.button}
-                    >{btnText}
-                    </Button>
-                    <Text style={styles.text}>Do not have an account ? Register Here</Text>
-                </ImageBackground>
-            </Layout>
+                    <View>
+                        <Button
+                            onPress={onPress()}
+                            size="giant" style={styles.loginButton}
+                        >{btnText}
+                        </Button>
+                        
+                        <View style={styles.socialButtonContainer}>
+                            <Button
+                                onPress={onPressGoogle()}
+                                size="giant"
+                                appearance={'ghost'}
+                                style={styles.button}
+
+                                icon={(style) => {
+                                    //alert(JSON.stringify(style))
+                                    return (
+                                        <Icon
+                                            name={'google'}
+                                            {...style}
+                                            tintColor={'#fff'}
+                                            width={30}
+                                            height={30}
+                                        />
+                                    )
+                                }
+                                }
+                            >
+                            </Button>
+                            <Button
+                                onPress={onPressFacebook()}
+                                size="giant"
+                                appearance={'ghost'}
+                                style={styles.button}
+                                icon={(style) => {
+                                    //alert(JSON.stringify(style))
+                                    return (
+                                        <Icon
+                                            name={'facebook'}
+                                            {...style}
+                                            tintColor={'#fff'}
+                                            width={30}
+                                            height={30}
+                                        />
+                                    )
+                                }
+                                }
+
+                            >
+                            </Button>
+                            <Button
+                                onPress={onPressTwitter()}
+                                size="giant"
+                                appearance={'ghost'}
+
+                                style={styles.button}
+
+                                icon={(style) => {
+                                    //alert(JSON.stringify(style))
+                                    return (
+                                        <Icon
+                                            name={'twitter'}
+                                            {...style}
+                                            tintColor={'#fff'}
+                                            width={30}
+                                            height={30}
+                                        />
+                                    )
+                                }
+                                }
+                            >
+                            </Button>
+                        </View>
+                        <TouchableOpacity>
+                            <Text style={styles.text}>Don't have an account ? Sign Up</Text>
+                        </TouchableOpacity>
+                    </View>
+                </Layout>
+            </ImageBackground>
         );
     }
 }
 
-
 const styles = StyleSheet.create({
     button: {
-        width: '75%',
-        position: 'absolute',
-        bottom: 40
+        marginTop: 10
     },
     wrapper: {
-        flex: 1
+        flex: 1,
+        alignItems: 'stretch'
+    },
+    titleContainer: {
+        backgroundColor: 'transparent',
+        alignItems: 'center',
+        height: 180,
+        justifyContent: 'center'
+    },
+    formContainer: {
+        paddingVertical: 20,
+        paddingHorizontal: 20,
+        justifyContent: 'space-between',
+        flexGrow: 1,
+        backgroundColor: 'transparent'
     },
     text: {
-        color: 'white',
         textAlign: 'center',
         alignSelf: 'center',
-        bottom: 10,
-        position: 'absolute'
+        marginTop: 20,
+        color: '#fff'
+    },
+    titleText: {
+        fontSize: 35,
+        fontWeight: 'bold',
+        color: '#fff'
     },
     input: {
-        width: '75%',
         marginTop: '2%'
+    },
+    socialButtonContainer: {
+        flexDirection: 'row',
+        justifyContent : 'space-around',
+        marginTop : '20%',
+        marginBottom: 20,
     }
 })
 
